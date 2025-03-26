2f39404f24d555a9490ad7c88c71a2b561bff55e
Aryan Goyal <goyal.aryan@gmail.com>
2025-03-26 19:42:09 +0530

    added alt

--- src/components/Box.js
+++ src/components/Box.js
@@ -6,51 +6,63 @@ import { motion} from "motion/react"
 export default function Box(props) {
   // const scrollProg = useScroll().scrollYProgress
   return (
-    <div className='myInfo'>
+    <div className="myInfo">
       <div className="image">
-        <a href={props.a} target="_blank" rel="noopener noreferrer"><motion.img height={props.height} width={props.width} src={props.src} 
-        initial={{
-          opacity:0
-        }}
-        whileInView={{
-          opacity:1
-        }}
-        transition={{
-          duration:1
-        }}
-        viewport={{
-              margin: "-50px"
-            }} /></a>
+        <a href={props.a} target="_blank" rel="noopener noreferrer">
+          <motion.img
+            alt=""
+            height={props.height}
+            width={props.width}
+            src={props.src}
+            initial={{
+              opacity: 0,
+            }}
+            whileInView={{
+              opacity: 1,
+            }}
+            transition={{
+              duration: 1,
+            }}
+            viewport={{
+              margin: "-50px",
+            }}
+          />
+        </a>
       </div>
-      <div className='column'>
+      <div className="column">
         <motion.h1
-        initial={{
-          opacity:0
-        }}
-        whileInView={{
-          opacity:1
-        }}
-        viewport={{
-          margin:"-50px"
-        }}
-        transition={{
-          duration:1
-        }}>{props.h1}</motion.h1>
+          initial={{
+            opacity: 0,
+          }}
+          whileInView={{
+            opacity: 1,
+          }}
+          viewport={{
+            margin: "-50px",
+          }}
+          transition={{
+            duration: 1,
+          }}
+        >
+          {props.h1}
+        </motion.h1>
         <motion.p
-        initial={{
-          opacity:0
-        }}
-        whileInView={{
-          opacity:1
-        }}
-        transition={{
-          duration:3
-        }}
-        viewport={{
-          amount:"-20px"}}>{props.p}</motion.p>
+          initial={{
+            opacity: 0,
+          }}
+          whileInView={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 3,
+          }}
+          viewport={{
+            amount: "-20px",
+          }}
+        >
+          {props.p}
+        </motion.p>
       </div>
-      
-      
     </div>
-  )
+  );
 }
